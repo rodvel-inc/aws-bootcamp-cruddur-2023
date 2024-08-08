e37f95edfae6852f23238bd5bca621c2679287fe
rodvel-inc <ajvelandiae@gmail.com>
2024-08-08 01:34:17 +0000

    Fixed communication between backend and frontend. Now the home feed page loads its content.

--- backend-flask/app.py
+++ backend-flask/app.py
@@ -63,16 +63,18 @@ origins = [frontend, backend]
 cors = CORS(
   app, 
   resources={r"/api/*": {"origins": origins}},
-  expose_headers="location,link",
-  allow_headers="content-type,if-modified-since",
+  headers=['Content-Type', 'Authorization'], 
+  #expose_headers="location,link",
+  expose_headers='Authorization',
+  #allow_headers="content-type,if-modified-since",
   methods="OPTIONS,GET,HEAD,POST"
 )
 
-@app.after_request
-def after_request(response):
-    timestamp = strftime('[%Y-%b-%d %H:%M]')
-    LOGGER.error('%s %s %s %s %s %s', timestamp, request.remote_addr, request.method, request.scheme, request.full_path, response.status)
-    return response
+##@app.after_request
+##def after_request(response):
+##    timestamp = strftime('[%Y-%b-%d %H:%M]')
+##    LOGGER.error('%s %s %s %s %s %s', timestamp, request.remote_addr, request.method, request.scheme, request.full_path, response.status)
+##    return response
 
 @app.route("/api/message_groups", methods=['GET'])
 def data_message_groups():
